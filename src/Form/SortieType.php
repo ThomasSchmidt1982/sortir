34c9926db9017138aa67d0835a2b80f18e0a192e
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2025-08-07 16:22:04 +0200

    creer, modifier, publier une sortie

---
+++ src/Form/SortieType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie',
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'widget' => 'choice',
+                'label' => 'Date et heure de la sortie'
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'widget' => 'choice',
+                'label' => 'Date limite d\'inscription',
+            ])
+            ->add('nbInscriptionMax', IntegerType::class, [
+                'label' => 'Nombre de places',
+            ])
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée (en minutes)',
+                'attr' => [
+                    'min' => 1, // valeur positive
+                ]
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label' => 'Description et infos',
+            ])
+            ->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+                'placeholder' => 'Choisissez un lieu',
+                'label' => 'Lieu',
+                'required' => true,
+            ])
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
